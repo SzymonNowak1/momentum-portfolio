e59ff9027e3dd86eaeadd06c3c5fa27d77487b50
SzymonNowak1 <31253648+SzymonNowak1@users.noreply.github.com>
2025-12-07 10:23:52 +0100

    universe.py

---
+++ src/universe.py
@@ -0,0 +1,19 @@
+def get_us_universe():
+    """
+    Tymczasowy, przykładowy zestaw spółek z SP500 do testów.
+    Później można tu podpiąć pełną listę SP500.
+    """
+    return [
+        "AAPL",
+        "MSFT",
+        "NVDA",
+        "META",
+        "GOOGL",
+        "AMZN",
+        "AVGO",
+        "COST",
+        "ADBE",
+        "ORLY",
+        "LLY",
+        "JPM",
+    ]
